2353b3d949dc819c9fbd0ffc6aa5cfedfe2bd707
Per Abich <per.abich@unikum.net>
2020-11-05 10:58:56 +0100

    Added progress bars

--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module github.com/UnikumAB/logmerge
 go 1.15
 
 require (
+	github.com/klauspost/compress v1.11.2 // indirect
+	github.com/klauspost/pgzip v1.2.5
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/pkg/errors v0.8.1
 	github.com/spf13/cobra v1.1.1
 	github.com/spf13/viper v1.7.1
+	github.com/vbauerster/mpb/v5 v5.3.0
 )
--- merge/merge.go
+++ merge/merge.go
@@ -2,7 +2,11 @@ package merge
 
 import (
 	"bufio"
-	"compress/gzip"
+	"bytes"
+	"encoding/binary"
+
+	gzip "github.com/klauspost/pgzip"
+	//"compress/gzip"
 	"io"
 	"log"
 	"net/http"
@@ -11,6 +15,8 @@ import (
 
 	"github.com/UnikumAB/logmerge/formats"
 	"github.com/pkg/errors"
+	"github.com/vbauerster/mpb/v5"
+	"github.com/vbauerster/mpb/v5/decor"
 )
 
 func Merge(outputFileName string, inputFiles []string) {
@@ -19,10 +25,28 @@ func Merge(outputFileName string, inputFiles []string) {
 		log.Fatalf("Cannot create parser: %v", err)
 	}
 	wg := &sync.WaitGroup{}
+	p := mpb.New(mpb.WithWaitGroup(wg))
 	outChan := writeFileLines(outputFileName, wg)
 	var inChans []<-chan formats.Line
-	for _, arg := range inputFiles {
-		inChans = append(inChans, readfile(arg, parser, wg))
+	for _, filename := range inputFiles {
+		bar := p.AddBar(0,
+			mpb.PrependDecorators(
+				// simple name decorator
+				decor.Name(filename),
+				// decor.DSyncWidth bit enables column width synchronization
+				decor.Percentage(decor.WCSyncSpace),
+			),
+			mpb.AppendDecorators(
+				// replace ETA decorator with "done" message, OnComplete event
+				decor.OnComplete(
+					// ETA decorator with ewma age of 60
+					decor.EwmaETA(decor.ET_STYLE_GO, 60), "done",
+				),
+			),
+			mpb.BarRemoveOnComplete(),
+		)
+
+		inChans = append(inChans, readfile(filename, parser, wg, bar))
 	}
 	lines := make([]*formats.Line, len(inChans))
 	var line *formats.Line
@@ -76,13 +100,14 @@ func fillLines(inChans []<-chan formats.Line, lines []*formats.Line) ([]*formats
 	return lines, inChans
 }
 
-func readfile(filename string, parser formats.LineReader, wg *sync.WaitGroup) <-chan formats.Line {
+func readfile(filename string, parser formats.LineReader, wg *sync.WaitGroup, bar *mpb.Bar) <-chan formats.Line {
 	inChan := make(chan formats.Line)
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
 		defer close(inChan)
-		isGzip, err := detectGzip(filename)
+		isGzip, size, err := detectGzip(filename)
+		bar.SetTotal(size, false)
 		if err != nil {
 			log.Printf("Failed to detect content type: %v", err)
 			return
@@ -99,39 +124,57 @@ func readfile(filename string, parser formats.LineReader, wg *sync.WaitGroup) <-
 			if err != nil {
 				log.Printf("Failed to create gzip reader for %v: %v", filename, err)
 			}
-			log.Printf("%v is a GZiped file", filename)
+			defer checkedClose(reader)
 			inputReader = reader
 		}
+
 		scanner := bufio.NewScanner(inputReader)
 		for scanner.Scan() {
 			text := scanner.Text()
+			bar.IncrBy(len([]byte(text)))
 			line, err := parser.ParseLine(text)
 			if err != nil {
 				log.Printf("Failed to parse line: %v", err)
 			}
 			inChan <- line
 		}
+		bar.SetTotal(bar.Current(), true)
 	}()
 	return inChan
 }
 
-func detectGzip(filename string) (bool, error) {
+func detectGzip(filename string) (bool, int64, error) {
 	file, err := os.Open(filename)
 	if err != nil {
-		return false, errors.WithMessagef(err, "Cannot open file %s", filename)
+		return false, 0, errors.WithMessagef(err, "Cannot open file %s", filename)
 	}
 	defer checkedClose(file)
 	buff := make([]byte, 512)
 	_, err = file.Read(buff)
 	if err != nil {
-		return false, errors.WithMessagef(err, "Failed reading from %s", filename)
+		return false, 0, errors.WithMessagef(err, "Failed reading from %s", filename)
 	}
 	contentType := http.DetectContentType(buff)
+	stat, err := file.Stat()
+	if err != nil {
+		return false, 0, errors.WithMessagef(err, "failed to get stats for %v", filename)
+	}
+	lastBytes := make([]byte, 4)
+	_, err = file.ReadAt(lastBytes, stat.Size()-4)
+	if err != nil {
+		return false, 0, errors.WithMessagef(err, "Failed to read last 4 bytes from %v", filename)
+	}
 	switch contentType {
 	case "application/x-gzip", "application/zip":
-		return true, nil
+		buf := bytes.NewBuffer(lastBytes)
+		var decompressedSize int32
+		err = binary.Read(buf, binary.LittleEndian, &decompressedSize)
+		if err != nil {
+			return false, 0, errors.WithMessagef(err, "Failed to decode filesize for %v", filename)
+		}
+		return true, int64(decompressedSize), nil
 	default:
-		return false, nil
+		return false, stat.Size(), nil
 	}
 }
 
